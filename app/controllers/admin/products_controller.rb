ad8679b18fa7a7ec59a9fba20c446061d5750a96
Edmund Yu <eyu1@ualberta.ca>
2021-11-11 05:31:17 +0000

    Add username, password example to .env_example, Implement http basic auth with all admin resources

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ProductsController < ApplicationController
-
-  def index
+   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  
+   def index
     @products = Product.order(id: :desc).all
   end
 
